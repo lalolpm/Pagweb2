7bd3560861bd20f7ad2edd403ecaf9baa44bc7f6
lalolpm <48661092+lalolpm@users.noreply.github.com>
2019-03-17 20:41:23 -0400

    Update Clientes.php

--- Clientes.php
+++ Clientes.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Proyecto Web </title>
+	<title> Proyecto Web 2</title>
 	<meta charset="utf-8">
 
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1,minimum-scale=1"> 
