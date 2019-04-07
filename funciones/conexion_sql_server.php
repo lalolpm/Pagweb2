ad962a39bbe728d9c57dfc53e8146e3a927344c5
lalolpm <48661092+lalolpm@users.noreply.github.com>
2019-04-07 19:51:07 -0400

    h

--- funciones/conexion_sql_server.php
+++ funciones/conexion_sql_server.php
@@ -4,13 +4,31 @@ function db_conectar()
 	$conexion = mssql_connect('DESKTOP-LI38OGE\SQLEXPRESS','sa','lalo2140305')
 		or die('Error al conectar: ' . mssql_error);
 		
-	mssql_select_db('Pagweb2');
+	mssql_select_db('clinica');
 	return $conexion;
 }
+
 function ejecutar_query($sql, $link)
 	{
 		$resultado = mssql_query($sql, $link);
 		return $resultado;
+	}
+
+function traer_fila($resultado)
+	{
+		$fila = mssql_fetch_row($resultado);
+		return $fila;
+	}
+
+
+	function ejecutar_query($sql, $link)
+	{
+		$resultado = mssql_query($sql, $link);
+		return $resultado;
+	}
+	function numero_filas($resultado)
+	{
+		return mssql_num_rows($resultado);
 	}
 
 	?>
