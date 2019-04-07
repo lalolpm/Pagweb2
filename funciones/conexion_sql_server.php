bc2fd296694f6c4c5046cf24905cebbaa4b45ec2
badboycrash <48659376+badboycrash@users.noreply.github.com>
2019-04-07 19:48:51 -0400

    Update conexion_sql_server.php

--- funciones/conexion_sql_server.php
+++ funciones/conexion_sql_server.php
@@ -1,16 +1,25 @@
 <?php
 function db_conectar()
 {
-	$conexion = mssql_connect('DESKTOP-LI38OGE\SQLEXPRESS','sa','lalo2140305')
-		or die('Error al conectar: ' . mssql_error);
+	$conexion = mssql_connect("LENOVO-PC\SQLEXPRESS", "xdd", "1234") or die ('No hubo conexión con la base de datos:' . mssql_error());
 		
 	mssql_select_db('Pagweb2');
 	return $conexion;
 }
+
 function ejecutar_query($sql, $link)
 	{
 		$resultado = mssql_query($sql, $link);
 		return $resultado;
 	}
 
-	?>
+function traer_fila($resultado)
+	{
+		$fila=mssql_fetch_row($resultado);
+		return $fila;
+	}
+
+
+
+?>
+
